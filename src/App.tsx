b9720b3a213038df6e91794ffc622ccde41ee59c
Joshua <joshuatanderson1@gmail.com>
2019-10-18 14:50:55 -0500

    Added contact button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, CssBaseline, Grid } from '@material-ui/core'
+import { Typography, CssBaseline, Grid, Button } from '@material-ui/core'
 import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import { theme } from './theme';
 
@@ -12,22 +12,26 @@ const App: React.FC = () => {
       {/* add background image with css blur */}
       <CssBaseline />
       <Grid container className="app" justify="center" alignItems="center">
-        <Grid item container spacing={3} sm={6} xs={10} className="white-box">
+        <Grid item container spacing={3} sm={6} xs={10} className="white-box" justify="center">
           <Grid item xs={12}>
             <Typography variant="h4" align="center">
               {/* add logo */}
               CLBD's new website is under construction.
             </Typography>
           </Grid>
-          <Grid className="imageCont" item xs={12}>
-              <img src={logo} alt="clbd-logo" />
+          <Grid className="grid-item-centers" item xs={10}>
+            <img src={logo} alt="clbd-logo" />
           </Grid>
           <Grid item xs={12}>
             <Typography variant="h5" align="center">
               Please pardon our dust.
             </Typography>
           </Grid>
-
+          <Grid item xs={12} className="grid-item-centers">
+            <Button href="mailto:clbd@lsu.edu">
+              contact us
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </ThemeProvider>
