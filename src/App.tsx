1341c18c0d2fde3ebb343488f810b761ed1ae472
Joshua <joshuatanderson1@gmail.com>
2019-10-18 10:42:41 -0500

    adding images

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography, CssBaseline, Grid } from '@material-ui/core'
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import { theme } from './theme';
+
+import './app.css'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      {/* add background image with css blur */}
+      <CssBaseline />
+      <Grid container className="app" justify="center" alignItems="center">
+        <Grid item container spacing={3} xs={6} className="white-box">
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center">
+              {/* add logo */}
+              CLBD's new website is under construction.
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <img src="./background.jpg" className="image" alt="clbd_logo" />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h5" align="center">
+              {/* add logo */}
+              Please pardon our dust.
+            </Typography>
+          </Grid>
+
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.tsx
@@ -0,0 +1,9 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#137FDC',
+		}
+	}
+});
